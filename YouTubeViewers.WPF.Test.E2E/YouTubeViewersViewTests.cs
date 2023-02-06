a5aca527b553877676d5561444a3aa409fc0c4e1
SingletonSean <singletonsean4@gmail.com>
2023-02-05 20:55:35 -0500

    add unit test for wpf component failures

---
+++ YouTubeViewers.WPF.Test.E2E/YouTubeViewersViewTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using YouTubeViewers.WPF.Views;
+
+namespace YouTubeViewers.WPF.Test.E2E
+{
+    [TestFixture]
+    public class YouTubeViewersViewTests
+    {
+        [Test]
+        public void Initialize_ShouldFailBecauseYouCanNotRenderWpfComponentsOffTheUiThread()
+        {
+            Assert.Throws<InvalidOperationException>(() => new YouTubeViewersView());
+        }
+    }
+}
